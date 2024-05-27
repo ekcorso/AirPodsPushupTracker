3a60d275a39e6509fd82269c8bcd752229dd9457
Emily Corso <ekcorso@gmail.com>
2024-05-26 21:09:53 -0700

    Add pitch data to chart view

--- APPushupTracker/PushupChartView.swift
+++ APPushupTracker/PushupChartView.swift
@@ -9,12 +9,18 @@ import SwiftUI
 import Charts
 
 struct PushupChartView: View {
-    @State private var data: [Double]  = [9, 6, 7, 4, 6, 2, 9, 6, 7, 4, 6, 2, 9, 6, 7, 4, 6, 2]
+    @State private var accellerationData: [Double]  = [9, 6, 7, 4, 6, 2, 9, 6, 7, 4, 6, 2, 9, 6, 7, 4, 6, 2]
+    @State private var pitchData = [1, 1.5, 1.2, 1, 1, 1, 0.8, 1.1, 1.02, 0.95, 1]
+    @State private var averagePitch = 0
     
     var body: some View {
-        Chart {
-            ForEach(Array(zip(data.indices, data)), id: \.0) { item in
-                LineMark(x: .value("Index", item.0), y: .value("Acceleration", item.1))
+        VStack {
+            Text("Average pitch: \(averagePitch)")
+                .padding(30)
+            Chart {
+                ForEach(Array(zip(accellerationData.indices, accellerationData)), id: \.0) { item in
+                    LineMark(x: .value("Index", item.0), y: .value("Acceleration", item.1))
+                }
             }
         }
     }
