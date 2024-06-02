04ee12c3c2efb4e9c5017c6e70ee2d12104e4866
Emily Corso <ekcorso@gmail.com>
2024-06-01 23:00:58 -0700

    Fix acceleration typo in CharView

--- APPushupTracker/PushupChartView.swift
+++ APPushupTracker/PushupChartView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 import Charts
 
 struct PushupChartView: View {
-    @State private var accellerationData: [Double]  = [9, 6, 7, 4, 6, 2, 9, 6, 7, 4, 6, 2, 9, 6, 7, 4, 6, 2]
+    @State private var accelerationData: [Double]  = [9, 6, 7, 4, 6, 2, 9, 6, 7, 4, 6, 2, 9, 6, 7, 4, 6, 2]
     @State private var pitchData = [1, 1.5, 1.2, 1, 1, 1, 0.8, 1.1, 1.02, 0.95, 1]
     @State private var averagePitch = 0
     
@@ -18,7 +18,7 @@ struct PushupChartView: View {
             Text("Average pitch: \(averagePitch)")
                 .padding(30)
             Chart {
-                ForEach(Array(zip(accellerationData.indices, accellerationData)), id: \.0) { item in
+                ForEach(Array(zip(accelerationData.indices, accelerationData)), id: \.0) { item in
                     LineMark(x: .value("Index", item.0), y: .value("Acceleration", item.1))
                 }
             }
