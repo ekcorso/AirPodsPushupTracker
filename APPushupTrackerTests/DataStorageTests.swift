7f387e3b50776bacc5e69f29f8a86643126053f3
Emily Corso <ekcorso@gmail.com>
2024-06-22 17:33:04 -0700

    Add testing target + getAveragePitch test

---
+++ APPushupTrackerTests/DataStorageTests.swift
@@ -0,0 +1,24 @@
+//
+//  DataStorageTests.swift
+//  APPushupTrackerTests
+//
+//  Created by Emily Corso on 6/22/24.
+//
+
+import XCTest
+@testable import APPushupTracker
+
+final class DataStorageTests: XCTestCase {
+    
+    func testGetAveragePitch() throws {
+        // Arrange
+        let pitches = [1, 1.5, 1.2, 0.95]
+        let expectedAveragePitch = 1.1625
+        
+        // Act
+        let result = DataStorage().getAveragePitch(pitches)
+        
+        // Assert
+        XCTAssertEqual(expectedAveragePitch, result)
+    }
+}
