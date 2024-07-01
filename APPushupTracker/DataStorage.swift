6689e33eee6e3b185a8f99514c839a82dd08d5a5
Emily Corso <ekcorso@gmail.com>
2024-06-30 18:50:28 -0700

    Refactor DataStorage property initialization

--- APPushupTracker/DataStorage.swift
+++ APPushupTracker/DataStorage.swift
@@ -9,16 +9,17 @@ import Foundation
 
 class DataStorage: ObservableObject {
     let defaults = UserDefaults.standard
-   
-    @Published var avgPitch: Double = 0 // This could be a computed property
-    @Published var accelerationData: [Double] = (UserDefaults.standard.array(forKey: Key.accelerationDataKey) as? [Double]) ?? [Double]()
-    
-    @Published var pitchData: [Double] = (UserDefaults.standard.array(forKey: Key.pitchDataKey) as? [Double]) ?? [Double]() {
-        didSet {
-            self.avgPitch = getAveragePitch(pitchData)
-        }
-    }
+  
+    @Published var avgPitch: Double = 0
+    @Published var accelerationData: [Double]
+    @Published var pitchData: [Double]
 
+    init() {
+        accelerationData = (UserDefaults.standard.array(forKey: Key.accelerationDataKey) as? [Double]) ?? [Double]()
+        pitchData = (UserDefaults.standard.array(forKey: Key.pitchDataKey) as? [Double]) ?? [Double]()
+        
+        avgPitch = getAveragePitch(pitchData)
+    }
     
     struct Key {
         static let accelerationDataKey = "accelerationData"
