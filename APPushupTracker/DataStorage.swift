1af943ad1625d93b0719957e811233cb1b40360e
Emily Corso <ekcorso@gmail.com>
2024-07-05 17:55:30 -0700

    Add methods to calculate avg up/down acceleration

--- APPushupTracker/DataStorage.swift
+++ APPushupTracker/DataStorage.swift
@@ -35,6 +35,30 @@ class DataStorage: ObservableObject {
         return Double(pitchSum/pitchCount)
     }
     
+    func getAvgUpAcceleration(_ data: [Double]) -> Double {
+        guard data.count != 0 else { return 0 }
+
+        let upAccData = data.filter() { $0 >= 0.3 }
+        // We can throw out 0 here because if the acc is 0 the user isn't moving anyway, so it's not relevant to the rep threshold
+        // Throwing out some of the other small values here. didUpdateAccelerationY is very sensitive
+        
+        let avgUpAcc = Double(upAccData.reduce(0, +)) / Double(upAccData.count)
+        
+        return avgUpAcc
+    }
+    
+    func getAvgDownAcceleration(_ data: [Double]) -> Double {
+        guard data.count != 0 else { return 0 }
+
+        let downAccData = data.filter() { $0 <= -0.7 }
+        // We can throw out 0 here because if the acc is 0 the user isn't moving anyway, so it's not relevant to the rep threshold
+        // Throwing out some of the other small values here. didUpdateAccelerationY is very sensitive
+
+        let avgDownAcc = Double(downAccData.reduce(0, +)) / Double(downAccData.count)
+        
+        return avgDownAcc
+    }
+    
     func savePitchData(_ data: [Double]) {
         defaults.setValue(data, forKey: Key.pitchDataKey)
         pitchData.append(contentsOf: data)
