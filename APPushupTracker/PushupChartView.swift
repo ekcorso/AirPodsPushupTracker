6d07a0a4b74aeb72dfc974651d2b461b02d3228b
Emily Corso <ekcorso@gmail.com>
2024-07-10 18:02:13 -0700

    Rename dataStorage to viewModel in PushupChartView

--- APPushupTracker/PushupChartView.swift
+++ APPushupTracker/PushupChartView.swift
@@ -9,24 +9,24 @@ import SwiftUI
 import Charts
 
 struct PushupChartView: View {
-    @Environment(PushupChartViewViewModel.self) private var dataStorage
+    @Environment(PushupChartViewViewModel.self) private var viewModel
     
     var body: some View {
         VStack {
-            Text("Average pitch: \(dataStorage.avgPitch)")
+            Text("Average pitch: \(viewModel.avgPitch)")
                 .padding(10)
-            Text("Average acceleration up: \(dataStorage.avgUpAcceleration)")
+            Text("Average acceleration up: \(viewModel.avgUpAcceleration)")
                 .padding(10)
-            Text("Average acceleration down: \(dataStorage.avgDownAcceleration)")
+            Text("Average acceleration down: \(viewModel.avgDownAcceleration)")
                 .padding(10)
             Button("Print data") {
-                print("The averagePitch is: \(dataStorage.avgPitch)")
-                print("The pitchData is: \(dataStorage.pitchData)")
-                print("The accelerationData is: \(dataStorage.accelerationData)")
+                print("The averagePitch is: \(viewModel.avgPitch)")
+                print("The pitchData is: \(viewModel.pitchData)")
+                print("The accelerationData is: \(viewModel.accelerationData)")
             }
             Text("Acceleration Data")
             Chart {
-                ForEach(Array(zip(dataStorage.accelerationData.indices, dataStorage.accelerationData)), id: \.0) { item in
+                ForEach(Array(zip(viewModel.accelerationData.indices, viewModel.accelerationData)), id: \.0) { item in
                     LineMark(x: .value("Index", item.0), y: .value("Acceleration", item.1))
                 }
             }
