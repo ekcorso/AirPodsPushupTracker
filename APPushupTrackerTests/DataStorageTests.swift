baed19b1cd3685d7126f3cd4a6a628d5f158dde0
Emily Corso <ekcorso@gmail.com>
2024-07-10 22:01:13 -0700

    Fix tests

--- APPushupTrackerTests/DataStorageTests.swift
+++ APPushupTrackerTests/DataStorageTests.swift
@@ -9,14 +9,15 @@ import XCTest
 @testable import APPushupTracker
 
 final class DataStorageTests: XCTestCase {
+    let dataStore = DataStore()
     
-    func testGetAveragePitch() throws {
+    func testGetAveragePitch() async throws {
         // Arrange
         let pitches = [1, 1.5, 1.2, 0.95]
         let expectedAveragePitch = 1.1625
         
         // Act
-        let result = PushupChartViewViewModel().getAveragePitch(pitches)
+        let result = await PushupChartViewViewModel(dataStore: dataStore).getAveragePitch(pitches)
         
         // Assert
         XCTAssertEqual(expectedAveragePitch, result)
