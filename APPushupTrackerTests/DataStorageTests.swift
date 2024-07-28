97483f98965deca34b93fca7b383c05016582fcd
Emily Corso <ekcorso@gmail.com>
2024-07-28 16:46:44 -0700

    Fix failing DataStore test for ViewModel update

--- APPushupTrackerTests/DataStorageTests.swift
+++ APPushupTrackerTests/DataStorageTests.swift
@@ -9,15 +9,13 @@ import XCTest
 @testable import APPushupTracker
 
 final class DataStorageTests: XCTestCase {
-    let dataStore = DataStore()
-    
     func testGetAveragePitch() async throws {
         // Arrange
         let pitches = [1, 1.5, 1.2, 0.95]
         let expectedAveragePitch = 1.1625
         
         // Act
-        let result = await PushupChartViewViewModel(dataStore: dataStore).getAveragePitch(pitches)
+        let result = PushupChartViewViewModel().getAveragePitch(pitches)
         
         // Assert
         XCTAssertEqual(expectedAveragePitch, result)
