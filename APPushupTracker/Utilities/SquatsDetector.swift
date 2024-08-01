efbb97c1e277f2577794402a88b479e26fdbaf7f
Emily Corso <ekcorso@gmail.com>
2024-08-01 15:38:04 -0700

    Draft SquatsDetector class

---
+++ APPushupTracker/Utilities/SquatsDetector.swift
@@ -0,0 +1,55 @@
+//
+//  SquatsDetector.swift
+//  APPushupTracker
+//
+//  Created by Emily Corso on 7/31/24.
+//
+
+import Foundation
+import SwiftUI
+
+@Observable
+class SquatsDetector: Detector {
+    override init() {
+        super.init()
+        
+        // TODO: Need to rethink these thresholds for Squats. Currently still detecting pushups
+        downThreshold = -0.7
+        upThreshold = 0.3
+        proneThreshold = -0.8
+        
+        motionManager.delegate = self
+    }
+    
+    // Must call this before utilizing this class
+    override func initializeData() {
+        accelerationData = dataStore.retrieveAccelerationData() ?? [Double]()
+        pitchData = dataStore.retrievePitchData() ?? [Double]()
+    }
+    
+    override func saveData() async {
+        await dataStore.savePitchData(pitchData)
+        await dataStore.saveAccelerationData(accelerationData)
+    }
+}
+
+// TODO: Need to retool this for Squats. Currently still detecting pushups
+extension SquatsDetector: MotionManagerDelegate {
+    func didUpdateAccelerationY(_ accelerationY: Double) {
+        accelerationData.append(accelerationY)
+        
+        if accelerationY < downThreshold && !isUpwardPhase {
+            // User is movintg downward in a pushup
+            isUpwardPhase = true
+        } else if accelerationY > upThreshold && isUpwardPhase {
+            incrementCount()
+            isUpwardPhase = false
+        }
+    }
+    
+    func didUpdatePitch(_ pitch: Double) {
+        pitchData.append(pitch)
+        
+        isValidPosition = pitch < proneThreshold
+    }
+}
