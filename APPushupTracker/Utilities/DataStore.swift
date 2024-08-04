fec6002094933d4398e2b99a73c3dcf7691c0e65
Emily Corso <ekcorso@gmail.com>
2024-08-03 17:06:24 -0700

    Remove unnecessary DataStore properties

--- APPushupTracker/Utilities/DataStore.swift
+++ APPushupTracker/Utilities/DataStore.swift
@@ -20,38 +20,24 @@ actor DataStore {
     
     private let defaults = UserDefaults.standard
     
-    private var pushupAccelerationData: [Double]
-    private var pushupPitchData: [Double]
-    
-    private var squatAccelerationData: [Double]
-    private var squatPitchData: [Double]
-    
     private init() {
-        self.pushupAccelerationData = (UserDefaults.standard.array(forKey: Key.pushupAccelerationDataKey) as? [Double]) ?? [Double]()
-        self.pushupPitchData = (UserDefaults.standard.array(forKey: Key.pushupAccelerationDataKey) as? [Double]) ?? [Double]()
         
-        self.squatAccelerationData = (UserDefaults.standard.array(forKey: Key.squatAccelerationKey) as? [Double]) ?? [Double]()
-        self.squatPitchData = (UserDefaults.standard.array(forKey: Key.squatPitchKey) as? [Double]) ?? [Double]()
     }
     
     func savePushupPitchData(_ data: [Double]) {
         defaults.setValue(data, forKey: Key.pushupPitchDataKey)
-        pushupPitchData.append(contentsOf: data)
     }
     
     func savePushupAccelerationData(_ data: [Double]) {
         defaults.setValue(data, forKey: Key.pushupAccelerationDataKey)
-        pushupAccelerationData.append(contentsOf: data)
     }
     
     func saveSquatPitchData(_ data: [Double]) {
         defaults.setValue(data, forKey: Key.squatPitchKey)
-        pushupPitchData.append(contentsOf: data)
     }
     
     func saveSquatAccelerationData(_ data: [Double]) {
         defaults.setValue(data, forKey: Key.squatAccelerationKey)
-        pushupAccelerationData.append(contentsOf: data)
     }
     
     nonisolated func retrievePushupPitchData() -> [Double]? {
